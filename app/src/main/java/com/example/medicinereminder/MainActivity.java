b4ab46563bce4f3dea9bd317807803a41003a5e8
kirito911 <master.aman2000@gmail.com>
2021-05-22 20:49:30 +0530

    added intent code

--- app/src/main/java/com/example/medicinereminder/MainActivity.java
+++ app/src/main/java/com/example/medicinereminder/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.medicinereminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv1,tv2,tv3;
+    Button btnSignup, btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, com.example.medicinereminder.Log_In.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, com.example.medicinereminder.sign_up.class);
+                startActivity(intent);
+            }
+        });
     }
 }
