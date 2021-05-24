cb5c620c42e8fc82f7a0cb39c03e9580e9635611
kapilkmr678@gmail.com <kapilkmr678@gmail.com>
2021-05-24 10:22:26 +0530

    "login and sign up added"

--- app/src/main/java/com/example/medicinereminder/HomepageActivity.java
+++ app/src/main/java/com/example/medicinereminder/HomepageActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Log_In extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_homepage);
     }
 }
--- app/src/main/java/com/example/medicinereminder/MainActivity.java
+++ app/src/main/java/com/example/medicinereminder/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.medicinereminder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mSignBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSignBtn = findViewById(R.id.signbtn);
+
+
+        mSignBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
     }
+
+
+
 }
